38dbd2219a916a699b7cf691ec6c7e256c899722
CalinFasie <calin79fc@yahoo.com>
2022-02-01 18:28:04 +0200

    message1

---
+++ helloWorld/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+//comment
+    public static void main(String... args){
+        System.out.println("Hello World! Have you worked with GIT before?");
+        System.out.println("Line added by Calin");
+    }
+}
